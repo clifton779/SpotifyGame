15725917a7ab8fc690b3c2314d52f05df9ede3b6
root <root@LAPTOP-QPC1V445.localdomain>
2022-04-04 17:32:39 -0400

    added route for querying api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import api
 
 
 rock = []
@@ -25,6 +26,29 @@ def index():
     # NB: DO NOT add an "index.html" file in your normal templates folder
     # Flask will stop serving this React page correctly
     return flask.render_template("index.html")
+
+
+@bp.route("/getsongs", methods=["POST", "GET"])
+def getSongs(genre):
+    arr = []
+    urls = []
+    names = []
+    if genre == "rock":
+        arr = rock
+    elif genre == "pop":
+        arr = pop
+    elif genre == "hiphop":
+        arr = hiphop
+    elif genre == "country":
+        arr = country
+    elif genre == "alternative":
+        arr = alternative
+    urls = api.getSongUrls(arr)
+    names = api.getSongTitles(arr)
+    jsondata = []
+    for url, name in zip(urls, names):
+        jsondata.append({"url": url, "name": name})
+    return flask.jsonify({"songs": jsondata})
 
 
 app.register_blueprint(bp)

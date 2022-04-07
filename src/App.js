72b561a964e48cec46ddcc1af51ca6df858ba5d8
root <root@LAPTOP-QPC1V445.localdomain>
2022-04-07 18:21:25 -0400

    changed import statement slightly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { Player } from './Player';
+import Player from './Player';
 
 function App() {
   const [music, setMusic] = useState([]);
